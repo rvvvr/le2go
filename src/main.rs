f74063c91b08061dcbe21a34df4cc0c6f3fc6a3d
rvvvr <me@visu.do>
2024-11-28 14:11:09 -0800

    small

--- src/main.rs
+++ src/main.rs
@@ -103,8 +103,8 @@ fn main() {
 	find_contours(&mask_blue, &mut blue_contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point::new(0, 0)).expect("Could not find blue contours!");
 	find_contours(&mask_red, &mut red_contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point::new(0, 0)).expect("Could not find red contours!");
 
-	let mut rightmost_red_contour: Vector<Point> = find_rightmost_contour(&red_contours, 5000);
-	let mut rightmost_blue_contour: Vector<Point> = find_rightmost_contour(&blue_contours, 5000);
+	let mut rightmost_red_contour: Vector<Point> = find_rightmost_contour(&red_contours, 1000);
+	let mut rightmost_blue_contour: Vector<Point> = find_rightmost_contour(&blue_contours, 1000);
 	let blue_rect = bounding_rect(&rightmost_blue_contour).expect("could not create a bounding box for blue contour!");
 	let red_rect = bounding_rect(&rightmost_red_contour).expect("could not create a bounding box for red contour!");
 
@@ -114,7 +114,7 @@ fn main() {
 	    (rightmost_red_contour, red_rect, Colour::Red)
 	};
 
-	if rightmost_rect.x < (540 - (rightmost_rect.width / 2)) {
+	if rightmost_rect.x < (240 - (rightmost_rect.width / 2)) {
 	    //not at assumed dropoff point yet, we shall wait.
 	    rectangle(&mut frame_in, rightmost_rect, VecN::from_array([255., 0., 0., 255.]), 1, LINE_8, 0).expect("could not draw preview rectangle!");
 	    
